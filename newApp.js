b2840d624fed66076f5d665d291de6e5042c7bb5
Sova <108209320+NceSova@users.noreply.github.com>
2024-06-15 17:54:49 +0300

    rewrite everything

--- newApp.js
+++ newApp.js
@@ -1,5 +1,5 @@
-function makeCell() {
-  let valueOfCell = "#";
+function makeCell(value) {
+  let valueOfCell = value;
   const setToken = (token) => {
     valueOfCell = token;
   };
@@ -16,23 +16,42 @@ function makePlayer(name, token) {
 const firstPlayer = makePlayer("me", "X");
 const secondPlayer = makePlayer("you", "O");
 
-const model = (function (player1, player2) {
+const view = (function () {
+  const takeInput = () => {
+    return +prompt();
+  };
+  const drawBoard = (boardArray) => {
+    console.log(
+      `${boardArray[0]} | ${boardArray[1]} | ${boardArray[2]}\n----------\n${boardArray[3]} | ${boardArray[4]} | ${boardArray[5]}\n----------\n${boardArray[6]} | ${boardArray[7]} | ${boardArray[8]}`
+    );
+  };
+  const gameOver = (player) => {
+    console.log(`${player.getPlayerName()} has Won!!!`);
+  };
+  const showCurPLayer = (player) => {
+    console.log(player.getPlayerName());
+  };
+  return {
+    takeInput,
+    drawBoard,
+    gameOver,
+    showCurPLayer,
+  };
+})();
+
+const model = (function (player1, player2, view) {
   let _board = [];
   for (let i = 0; i < 9; i++) {
-    _board[i] = makeCell();
+    _board[i] = makeCell(i);
   }
   const getBoard = () => _board.map((cell) => cell.getToken());
 
-  let _curPlayer = player1;
-  const _changeCurPlayer = () => {
-    _curPlayer = _curPlayer === player1 ? player2 : player1;
+  let curPlayer = player1;
+  const changeCurPlayer = () => {
+    curPlayer = curPlayer === player1 ? player2 : player1;
   };
 
-  let _gameOver = false;
-  const setGameOver = () => (_gameOver = true);
-  const isGameOver = () => _gameOver;
-
-  const _check = (player, index1, index2, index3) => {
+  const check = (player, index1, index2, index3) => {
     if (
       _board[index1].getToken() === player.getPlayerToken() &&
       _board[index2].getToken() === player.getPlayerToken() &&
@@ -44,7 +63,7 @@ const model = (function (player1, player2) {
     }
   };
 
-  const _winningIndexes = [
+  const winningIndexes = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -55,12 +74,9 @@ const model = (function (player1, player2) {
     [2, 4, 6],
   ];
 
-  const isPlayerWinning = (player) => {
-    console.log("tryWinner");
-    _winningIndexes.forEach((element) => {
-      if (_check(player, element[0], element[1], element[2])) {
-        console.log("winner is true");
-        setGameOver();
+  const isWon = (player) => {
+    winningIndexes.forEach((element) => {
+      if (check(player, element[0], element[1], element[2])) {
         return true;
       } else {
         return false;
@@ -69,7 +85,10 @@ const model = (function (player1, player2) {
   };
 
   const makeAMove = (index, player) => {
-    if (_board[index].getToken() === "#") {
+    if (
+      _board[index].getToken() !== player1.getPlayerToken() &&
+      _board[index].getToken() !== player2.getPlayerToken()
+    ) {
       _board[index].setToken(player.getPlayerToken());
       return true;
     } else {
@@ -77,43 +96,25 @@ const model = (function (player1, player2) {
     }
   };
 
-  const playRound = () => {
-    let moveIndex = +prompt();
-    let move = makeAMove(moveIndex, _curPlayer);
-    console.log(move);
-    while (move === false) {
-      console.log("bruh");
-      move = makeAMove(+prompt(), _curPlayer);
-    }
-    if (isPlayerWinning(_curPlayer)) {
-      setGameOver();
-      console.log("someone won!");
-      return `${_curPlayer.getPlayerName()} has won!`;
-    } else {
-      _changeCurPlayer();
-      return "playing...";
+  const play = () => {
+    gameOn = true;
+    while (gameOn) {
+      view.showCurPLayer(curPlayer);
+      view.drawBoard(getBoard());
+      let move = view.takeInput();
+      makeAMove(move, curPlayer);
+      if (isWon(curPlayer)) {
+        view.gameOver(curPlayer);
+        gameOn = false;
+      } else {
+        changeCurPlayer();
+      }
     }
   };
 
   return {
-    setGameOver,
-    _gameOver,
-    playRound,
-    isGameOver,
-    getBoard,
+    play,
   };
-})(firstPlayer, secondPlayer);
+})(firstPlayer, secondPlayer, view);
 
-const drawBoard = () => {
-  boardArray = model.getBoard();
-  return `${boardArray[0]} ${boardArray[1]} ${boardArray[2]}\n${boardArray[3]} ${boardArray[4]} ${boardArray[5]}\n${boardArray[6]} ${boardArray[7]} ${boardArray[8]}`;
-};
-
-console.log("start");
-while (model._gameOver === false) {
-  console.log("new round");
-  const sd = model.playRound();
-  console.log(drawBoard());
-  console.log(`gameover is ${model._gameOver}`);
-  console.log(`status is ${sd}`);
-}
+model.play();
